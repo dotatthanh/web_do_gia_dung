b67afe73eb92f4ffcdc751c06aad4a1cef599b43
Do Tat Thanh <dotatthanh@Mac.lan>
2025-12-08 19:58:27 +0700

    fix logo

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -14,29 +14,15 @@
     <div class="section" id="slider-wp">
         <div class="section-detail">
             <div class="item">
-                <img src="{{ asset('frontend/theme/image/slider/slider_1.png') }}" alt="">
+                <img src="{{ asset('frontend/theme/image/slider_1.jpg') }}" alt="">
             </div>
             <div class="item">
-                <img src="{{ asset('frontend/theme/image/slider/slider_2.png') }}" alt="">
+                <img src="{{ asset('frontend/theme/image/slider_2.jpg') }}" alt="">
             </div>
             <div class="item">
-                <img src="{{ asset('frontend/theme/image/slider/slider_3.png') }}" alt="">
+                <img src="{{ asset('frontend/theme/image/slider_3.jpg') }}" alt="">
             </div>
-            <div class="item">
-                <img src="{{ asset('frontend/theme/image/slider/slider_4.png') }}" alt="">
-            </div>
-            <div class="item">
-                <img src="{{ asset('frontend/theme/image/slider/slider_5.png') }}" alt="">
-            </div>
-            <div class="item">
-                <img src="{{ asset('frontend/theme/image/slider/slider_6.png') }}" alt="">
-            </div>
-            <div class="item">
-                <img src="{{ asset('frontend/theme/image/slider/slider_7.jpg') }}" alt="">
-            </div>
-            <div class="item">
-                <img src="{{ asset('frontend/theme/image/slider/slider_8.jpg') }}" alt="">
-            </div>
+            
         </div>
     </div>
     <div class="section" id="support-wp">
